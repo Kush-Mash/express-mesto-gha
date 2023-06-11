6e3d59c7da47d78418ae5bf3d95c0168f65493c9
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-06-11 19:11:44 +0300

    refactor: пройти тесты

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const signToken = require('../utils/jwtAuth').signToken;
+const { signToken } = require('../utils/jwtAuth');
 
 const {
   HTTP_STATUS_OK,
