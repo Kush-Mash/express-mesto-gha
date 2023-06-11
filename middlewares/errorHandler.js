a10e849c67f64016be2ea1d8205b014a563db2c4
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-06-11 18:06:50 +0300

    refactor: пройти test_config

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,6 @@ const HTTP_STATUS_INTERNAL_SERVER_ERROR = require('../utils/constants');
 
 const errorHandler = (err, req, res, next) => {
   const { statusCode = HTTP_STATUS_INTERNAL_SERVER_ERROR, message = 'Ошибка сервера' } = err;
-  console.log('Дефолтный обработчик ошибок:', err);
   res.status(statusCode)
     .send({ message });
 
