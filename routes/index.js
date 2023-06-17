e954336db01b9a9317eb212eb39997ead0c502f5
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-06-17 13:39:31 +0300

    refactor: создать логгеры запросов и ошибок

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const usersController = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validateUserCreate, validateLogin } = require('../middlewares/validate');
 const NotFoundError = require('../errors/NotFoundError');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+
+router.use(requestLogger);
 
 router.post('/signup', validateUserCreate, usersController.createUser);
 router.post('/signin', validateLogin, usersController.login);
@@ -14,5 +17,7 @@ router.use('/cards', auth, cardRouter);
 router.use('*', auth, () => {
   throw new NotFoundError('Запрашиваемая страница не найдена');
 });
+
+router.use(errorLogger);
 
 module.exports = router;
